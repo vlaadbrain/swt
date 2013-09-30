b3c42863984401676e8441b51786828299838c2f
Carlos J. Torres <vlaadbrain@gmail.com>
2013-09-30 14:35:37 +0000

    configuration, fonts, colors etc...

---
+++ config.h
@@ -0,0 +1,11 @@
+/* See LICENSE file for copyright and license details. */
+
+/* appearance */
+static const char font[]            = "-*-terminus-medium-r-*-*-14-*-*-*-*-*-*-*";
+static const char normbordercolor[] = "#073642";
+static const char normbgcolor[]     = "#002936";
+static const char normfgcolor[]     = "#93a1a1";
+static const char selbordercolor[]  = "#cb4b16";
+static const char selbgcolor[]      = "#073642";
+static const char selfgcolor[]      = "#93a1a1";
+
