045b440aa4eb1fb67daca5b989e9c2760326df22
Carlos J. Torres <vlaadbrain@gmail.com>
2013-09-30 14:34:58 +0000

    added initial setup for X11 and X11 file descriptor to monitor for XEvents

--- swt.c
+++ swt.c
@@ -1,8 +1,4 @@
-/*
- * See LICENSE file for copyright and license details.
- */
-
-#include <errno.h>
+/* * See LICENSE file for copyright and license details.  */ #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
@@ -16,6 +12,8 @@
 #include <stdarg.h>
 #include <stdbool.h>
 #include <libgen.h>
+#include <X11/cursorfont.h>
+#include <X11/Xutil.h>
 
 #include "drw.h"
 #include "util.h"
@@ -27,28 +25,59 @@
 
 #define PING_TIMEOUT 300
 
+enum { CurNormal, CurResize, CurMove, CurLast }; /* cursor */
+enum { SchemeNorm, SchemeSel, SchemeLast }; /* color schemes */
+
+
+static void buttonpress(const XEvent *ev);
 static void closefifo(void);
 static void createfifo(void);
 static void createout(void);
 static void dumptree(void);
+static void keypress(const XEvent *ev);
 static void noop(void);
 static void procinput(void);
 static void procadd(char *attrs);
 static void procremove(char *attrs);
 static void procshow(char *attrs);
 static void procwindow(char *attrs);
+static void procx11events(void);
 static void resetfifo(void);
 static void run(void);
+static void setup(void);
 static void usage(void);
 static void quit(void);
 static void writeout(const char *msg, ...);
 
+/* variables */
 char *argv0;
 
 static int infd = -1;
 static int winfd = -1; /* don't write here, it prevents EOF */
+static int x11fd = -1;
+
+static int screen;
+static void (*handler[LASTEvent]) (const XEvent *) = {
+	[ButtonPress] = buttonpress,
+	[KeyPress] = keypress,
+};
+
+static int sw, sh, wx, wy, ww, wh;
 static FILE *outfile;
 static char *in = NULL, *out = NULL;
+static Display *dpy;
+static Window root, window;
+static Drw *drw;
+static Fnt *fnt;
+static Cur *cursor[CurLast];
+static ClrScheme scheme[SchemeLast]; 
+
+#include "config.h"
+
+void
+buttonpress(const XEvent *ev) {
+	writeout("button pressed\n");
+}
 
 void
 closefifo(void) {
@@ -72,6 +101,7 @@ createfifo(void) {
 		perror("unable to open input fifo for reading");
 		quit();
 	}
+
 	if((winfd = open(in, O_WRONLY, 0)) == -1) {
 		perror("unable to open input fifo for writting");
 		quit();
@@ -89,6 +119,11 @@ createout(void) {
 void
 dumptree(void) {
 	writeout("CAN'T DUMP TREE YET\n");
+}
+
+void
+keypress(const XEvent *ev) {
+	writeout("key pressed\n");
 }
 
 void
@@ -167,6 +202,30 @@ procshow(char *attrs) {
 void
 procwindow(char *attrs) {
 	writeout("window request processing: %s\n", attrs);
+	XClassHint class_hint;
+
+	window = XCreateSimpleWindow(dpy, root, wx, wy, ww, wh, 0,
+			scheme[SchemeNorm].fg->rgb, scheme[SchemeNorm].bg->rgb);
+	XSelectInput(dpy, window, ButtonPressMask|KeyPressMask);
+
+	class_hint.res_name = "SWT";
+	class_hint.res_class = attrs;
+
+	XSetClassHint(dpy, window, &class_hint);
+
+	XMapWindow(dpy, window);
+	XSync(dpy, False);
+}
+
+void
+procx11events(void) {
+	XEvent ev;
+
+	while(XPending(dpy)) {
+		XNextEvent(dpy, &ev);
+		if(handler[ev.type])
+			handler[ev.type](&ev);
+	}
 }
 
 void
@@ -194,6 +253,7 @@ run(void) {
 	time_t last_response;
 
 	last_response = time(NULL);
+	x11fd = XConnectionNumber(dpy);
 
 	for(;;) {
 		int i, nfds = 0;
@@ -202,12 +262,13 @@ run(void) {
 
 		FD_ZERO(&rd);
 		FD_SET(infd, &rd);
+		FD_SET(x11fd, &rd);
 		nfds = MAX(nfds, infd);
+		nfds = MAX(nfds, x11fd);
 
 		i = select(nfds + 1, &rd, NULL, NULL, &tv);
 
-		if(i == -1 && errno == EINTR) 
-			continue;
+		if(i == -1 && errno == EINTR) continue;
 
 		if(i == -1) {
 			perror("swt: error on select()");
@@ -222,8 +283,37 @@ run(void) {
 				last_response = time(NULL);
 				procinput();
 			}
+			if(FD_ISSET(x11fd, &rd)) {
+				last_response = time(NULL);
+				procx11events();
+			}
 		}
 	}
+}
+
+void setup(void) {
+	screen = DefaultScreen(dpy);
+	root = RootWindow(dpy, screen);
+	fnt = drw_font_create(dpy, font);
+	sw = DisplayWidth(dpy, screen);
+	sh = DisplayHeight(dpy, screen);
+	wx = 0;
+	wy = 0;
+	ww = 800;
+	wh = 600;
+	drw = drw_create(dpy, screen, root, sw, sh);
+	drw_setfont(drw, fnt);
+
+	cursor[CurNormal] = drw_cur_create(drw, XC_left_ptr);
+	cursor[CurResize] = drw_cur_create(drw, XC_sizing);
+	cursor[CurMove] = drw_cur_create(drw, XC_fleur);
+
+	scheme[SchemeNorm].border = drw_clr_create(drw, normbordercolor);
+	scheme[SchemeNorm].bg = drw_clr_create(drw, normbgcolor);
+	scheme[SchemeNorm].fg = drw_clr_create(drw, normfgcolor);
+	scheme[SchemeSel].border = drw_clr_create(drw, selbordercolor);
+	scheme[SchemeSel].bg = drw_clr_create(drw, selbgcolor);
+	scheme[SchemeSel].fg = drw_clr_create(drw, selfgcolor);
 }
 
 void
@@ -260,11 +350,14 @@ main(int argc, char *argv[]) {
 
 	if(!in || !out) usage();
 
+	if(!(dpy = XOpenDisplay(NULL)))
+		die("swt: cannot open display\n");
+
+	setup();
 	createfifo();
 	createout();
-
 	run();
-
-	return 0;
+	XCloseDisplay(dpy);
+	return EXIT_SUCCESS;
 }
 
