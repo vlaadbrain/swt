24ad77312af2d53f578ecf2ac96ebaf06016134f
Carlos J. Torres <vlaadbrain@gmail.com>
2013-12-14 17:00:06 -0500

    another cludge hack to check box creation and resize

---
+++ test/boxes.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#crude but eventually i'll see a something out of it
+
+BIN="../swt"
+IN="./in"
+OUT="./out"
+
+exec $BIN -i $IN -o $OUT &
+
+SWT_PID=$!
+
+cat /dev/null > $OUT
+
+echo "window testing This is a new window" > $IN
+sleep 1
+WIN_ID=`grep "window testing" $OUT | awk '{print $3}'`
+echo "WIN_ID=${WIN_ID}"
+sleep 1
+echo "add testing hbox these are the attrs" > $IN
+sleep 2
+echo "dump" > $IN
+sleep 2
+xdotool keydown --window $WIN_ID ctrl+q
+
